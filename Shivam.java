13e418f6454ab3c2326cdaf1523ae41bf69874ff
Shivam Korpade <shivamkorpade985@gmail.com>
2025-03-25 00:46:17 +0530

    Change in notifiacation main

--- Shivam.java
+++ Shivam.java
@@ -1,7 +1,7 @@
 class Shivam{
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hi");
         m1();
     }
    public static void m1()
